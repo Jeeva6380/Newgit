3bccf21456af875301fa46788c3298b9bef2e3c4
Jeeva6380 <Jeeva2551@gmail.com>
2024-01-11 13:18:38 +0530

    Modified

---
+++ Hello.java
@@ -0,0 +1,12 @@
+package Newgit;
+import java.util.Scanner;
+
+public class Hello{
+    public static void main(String []args){
+      Scanner sc = new Scanner(System.in);
+      int k = sc.nextInt();
+      if(k<=18){
+             System.out.println("Minor");
+      }
+    }
+}
